e90feb3a129a1c981ca9ff4d0110ca7c80007d5d
Ayodele Olaosebikan <ayodeleolaosebikan59@gmail.com>
2025-06-25 18:50:29 +0100

    Files on index page sorted out better and all requires now put on index page

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 require '../helpers.php';
+require basePath('/views/partials/head.php');
+require basePath('/views/partials/nav.php');
 
 require basePath('/views/home.view.php');
+require basePath('/views/partials/footer.php');
 
