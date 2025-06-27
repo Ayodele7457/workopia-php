f6bce42cc4cc32650443ed05115d14ecb3277a19
Ayodele Olaosebikan <ayodeleolaosebikan59@gmail.com>
2025-06-27 14:07:01 +0100

    set up the very first basic router, now understand how routers work in detail and the role views and controllers and public does

--- helpers.php
+++ helpers.php
@@ -9,33 +9,59 @@ function basePath($path = '') : string {
 }
 
 /**
- * 
+ * Load a View
  * @param string $name
  * @return void
  */
-
  function loadView($name){
     $filePath = basePath("views/{$name}.view.php");
     if (file_exists($filePath)){
         require $filePath;
     } else {
-        echo "The View: $name was not found bro";
+        echo "The View: $name was not found";
     }
  }
 
 
 /**
- * 
+ * Load a Partial
  * @param string $name
  * @return void
  */
-
  function loadPartial($name){
     $filePath = basePath("views/partials/{$name}.php");
-
     if (file_exists($filePath)){
         require $filePath;
     } else {
-        echo "The Partial: $name was not found bro";
+        echo "The Partial: $name was not found";
     }
  }
+
+
+
+ /**
+  * Inspect Function for Troublehooting
+  * @param mixed $value
+  * @return void
+  *
+  */
+  function inspect($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '<pre>';
+  }
+
+
+ /**
+  * Inspect and die Function for Troublehooting
+  * @param mixed $value
+  * @return void
+  *
+  */
+  function inspectAndDie($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '<pre>';
+    die();
+  }
+
