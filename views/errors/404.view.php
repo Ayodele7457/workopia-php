f1ec0da51c914c053cda3ecc0fee71d49f92276a
Ayodele Olaosebikan <ayodeleolaosebikan59@gmail.com>
2025-06-30 16:43:42 +0100

    Minor chnges like setting up the 404 page view

--- views/errors/404.view.php
+++ views/errors/404.view.php
@@ -1,1 +1,20 @@
-yepa!
+<?php 
+include_once '../helpers.php';
+
+loadPartial('head');
+loadPartial('nav');
+loadPartial('top-banner');
+
+?>
+
+<section>
+      <div class="container mx-auto p-4 mt-4">
+         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">404 Error</div>
+         <p class="text-center text-2xl mb-4">
+            This page does not exist
+         </p>
+      </div>
+</section>
+
+<?php 
+loadPartial('footer');
